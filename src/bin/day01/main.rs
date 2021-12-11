dc8f4c9016310aae76af9c27e495a2bd907b4d6f
eric <eric@sanctuary.ai>
2021-12-11 14:33:35 -0800

    day 01

---
+++ src/bin/day01/main.rs
@@ -0,0 +1,48 @@
+
+use libaoc::libaoc::*;
+
+fn count_increases(inputs: &Vec<i32>) -> i32
+{
+    let mut count = 0_i32;
+    // let mut prev = inputs.get(0);
+
+    for i in 1..inputs.len()
+    {
+        if inputs.get(i-1) < inputs.get(i)
+        {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn count_3sum_increases(inputs: &Vec<i32>) -> i32
+{
+    let mut count = 0_i32;
+
+    for i in 3..inputs.len()
+    {
+        let trailing = inputs.get(i-3);
+        let leading = inputs.get(i);
+
+        if leading > trailing
+        {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn main()
+{
+    let filename = "src/bin/day01/input.txt";
+    let inputs: Vec<i32> = parse_file::<i32>(&filename);
+
+    let count = count_increases(&inputs);
+    println!("part 1: {}", count);
+
+    let count_2 = count_3sum_increases(&inputs);
+    println!("part 2: {}", count_2);
+}
---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+pub mod libaoc {
+
+    use std::fmt::Display;
+    use std::fs;
+    use std::str::FromStr;
+
+    pub fn log_vec<T: Display>(vec: Vec<T>)
+    {
+        for val in vec
+        {
+            println!("{}", val);
+        }
+    }
+
+    pub fn parse_str<T>(string: &str) -> T
+        where T: FromStr,  <T as std::str::FromStr>::Err : std::fmt::Debug
+    {
+        let ret = string.parse::<T>().unwrap();
+        ret
+    }
+
+    pub fn parse_file<T>(filename: &str) -> Vec<T>
+        where T: FromStr,  <T as std::str::FromStr>::Err : std::fmt::Debug
+    {
+
+        let mut ret: Vec<T> = Vec::new();
+        let contents = fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+        for substring in contents.split('\n')
+        {
+            ret.push(parse_str::<T>(&substring));
+        }
+
+        ret
+    }
+
+}
+
+// mod libaoc;
