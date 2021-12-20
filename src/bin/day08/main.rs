fa5ec6c40a70873cdd2cb44cd8113b7a374603e7
eric <ewells@ualberta.ca>
2021-12-19 23:06:24 -0800

    fixed day08, wow

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -1,6 +1,8 @@
 use regex::Regex;
 use std::fs;
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
+use bimap::BiMap;
 
 type Problem = (Vec<Vec<u8>>, Vec<Vec<u8>>);
 type ProblemSet = Vec<Problem>;
@@ -18,6 +20,7 @@ fn parse_input(filename: &str) -> ProblemSet
 
     for line in lines
     {
+
         let mut patterns: Vec<Vec<u8>> = Vec::new();
         let mut outputs: Vec<Vec<u8>> = Vec::new();
 
@@ -27,10 +30,14 @@ fn parse_input(filename: &str) -> ProblemSet
         for mat in mats
         {
             if n < 10 {
-                patterns.push(String::from(mat.as_str()).into_bytes());
+                let mut tmp = String::from(mat.as_str()).into_bytes();
+                tmp.sort();
+                patterns.push(tmp);
             }
             else{
-                outputs.push(String::from(mat.as_str()).into_bytes());
+                let mut tmp = String::from(mat.as_str()).into_bytes();
+                tmp.sort();
+                outputs.push(tmp);
             }
             n += 1;
         }
@@ -63,202 +70,113 @@ fn count_unique(problems: &ProblemSet) -> u64
     count
 }
 
-
-/// holds all elements
-#[derive(Clone)]
-struct record
+fn solve_problems_2(problems: &ProblemSet) -> u64
 {
-    symbols: Vec<symbol>,
-    solutions: Vec<Vec<u8>>,
-    solved: Vec<u8>
-}
+    let mut ret: u64 = 0;
 
-impl Default for record
-{
-    fn default() -> record
+    for problem in problems
     {
-        let symbols: Vec<u8> = vec![97, 98, 99, 100, 101, 102, 103];
-        let solutions: Vec<String> = vec![
-            "cf".to_string(),
-            "acf".to_string(),
-            "bcdf".to_string(),
-            "acdeg".to_string(),
-            "acdfg".to_string(),
-            "abdfg".to_string(),
-            "abcefg".to_string(),
-            "abdefg".to_string(),
-            "abcdfg".to_string(),
-            "abcdefg".to_string()
-        ];
+        let mut pattern_2_num_m: BiMap<Vec<u8>, u8> = BiMap::new();
 
-        let mut ret: record = record{
-            symbols: Vec::new(),
-            solutions: Vec::new(),
-            solved: Vec::new()
-        };
-
-        for symbol in &symbols
-        {
-            ret.symbols.push(symbol {value: *symbol, candidates: symbols.clone(), solved: false});
-        }
-
-        for solution in solutions
-        {
-            ret.solutions.push(solution.into_bytes());
-        }
-
-        ret
-    }
-}
-
-/// holds an encrypted value, and the solution candidates
-#[derive(Clone)]
-struct symbol
-{
-    candidates: Vec<u8>,
-    value: u8,
-    solved: bool
-}
-
-
-
-fn solve_problem(problem: &mut Problem)
-{
-    let mut rec: record = record::default();
-
-    // while not solved
-    while !check_solved(&mut rec)
-    {
-        // iterate over each pattern
         for pattern in &problem.0
         {
-            let num_unique = rec.solutions.iter().fold(0, |acc, x| {if x.len() == pattern.len() {return acc + 1} acc});
-
-            if num_unique == 1
+            match pattern.len()
             {
-                let sol = rec.solutions.iter().find(|x| x.len() == pattern.len()).unwrap().clone();
-                update_from_unique(&mut rec, pattern, &sol);
+                2 => {
+                    pattern_2_num_m.insert(pattern.clone(), 1);
+                },
+                3 => {
+                    pattern_2_num_m.insert(pattern.clone(), 7);
+                },
+                4 => {
+                    pattern_2_num_m.insert(pattern.clone(), 4);
+                }
+                7 => {
+                    pattern_2_num_m.insert(pattern.clone(), 8);
+                }
+                _ => {}
             }
         }
 
-        update_rec(&mut rec);
-        update_problem(&mut rec, problem);
-    }
-}
-
-
-/// take matching symbol chars from unique length puzzle and answer and update record
-fn update_from_unique(rec: &mut record, pattern: &Vec<u8>, symbol_ans: &Vec<u8>)
-{
-
-    if pattern.len() != symbol_ans.len()
-    {
-        panic!("bad call to update_from_unique");
-    }
-
-    for i in 0..rec.symbols.len()
-    {
-        if pattern.contains(&rec.symbols[i].value)
+        for pattern in &problem.0
         {
-            rec.symbols[i].candidates.retain(|x| symbol_ans.contains(&x));
-        }
-        else
-        {
-            rec.symbols[i].candidates.retain(|x| !symbol_ans.contains(&x));
-        }
-    }
-
-    rec.solutions.retain(|x| x != symbol_ans);
-}
-
-fn update_rec(rec: &mut record)
-{
-    for i in 0..rec.symbols.len()
-    {
-        if rec.symbols[i].candidates.len() == 1 && !rec.symbols[i].solved
-        {
-            rec.symbols[i].solved = true;
-            rec.solved.push(rec.symbols[i].candidates[0]);
-
-            let tmp = rec.symbols[i].candidates[0];
-
-            for j in 0..rec.symbols.len()
+            match pattern.len()
             {
-                if j == i {continue;}
+                5=> {   // 2, 3, 5
+                    let four = pattern_2_num_m.get_by_right(&4).unwrap();
+                    let seven = pattern_2_num_m.get_by_right(&7).unwrap();
 
-                rec.symbols[j].candidates.retain(|y| *y != tmp);
-            }
+                    let four_sum = four.iter().fold(0, |acc, x| if pattern.contains(x){return acc +1} else{return acc});
+                    let seven_sum = seven.iter().fold(0, |acc, x| if pattern.contains(x){return acc +1} else{return acc});
 
-            for sol in &mut rec.solutions
-            {
-                sol.retain(|x| *x != rec.symbols[i].candidates[0]);
-            }
-        }
-    }
-}
+                    if four_sum == 2
+                    {
+                        pattern_2_num_m.insert(pattern.clone(), 2);
+                    }
+                    else
+                    {
+                        if seven_sum == 3
+                        {
+                            pattern_2_num_m.insert(pattern.clone(), 3);
+                        }
+                        else
+                        {
+                            pattern_2_num_m.insert(pattern.clone(), 5);
+                        }
+                    }
 
-fn update_problem(rec: &mut record, problem: &mut Problem)
-{
-    for i in 0..rec.symbols.len()
-    {
-        if rec.symbols[i].solved
-        {
-            for pattern in &mut problem.0
-            {
-                pattern.retain(|x| *x != rec.symbols[i].value);
-            }
-        }
-    }
+                },
+                6 => {  // 0, 6, 9
+                    let four = pattern_2_num_m.get_by_right(&4).unwrap();
+                    let seven = pattern_2_num_m.get_by_right(&7).unwrap();
 
-}
+                    let four_sum = four.iter().fold(0, |acc, x| if pattern.contains(x){return acc +1} else{return acc});
+                    let seven_sum = seven.iter().fold(0, |acc, x| if pattern.contains(x){return acc +1} else{return acc});
 
-fn check_solved(rec: &mut record) -> bool
-{
-    let mut ret = true;
-
-    for element in &rec.symbols
-    {
-        if element.candidates.len() != 1
-        {
-            return false
-        }
-    }
-
-    for i in 0..rec.solutions.len()
-    {
-        let mut tmp = true;
-        for val in &rec.solutions[i]
-        {
-            if !rec.solved.contains(val)
-            {
-                tmp = false;
+                    if seven_sum == 2
+                    {
+                        pattern_2_num_m.insert(pattern.clone(), 6);
+                    }
+                    else
+                    {
+                        if four_sum == 4
+                        {
+                            pattern_2_num_m.insert(pattern.clone(), 9);
+                        }
+                        else
+                        {
+                            pattern_2_num_m.insert(pattern.clone(), 0);
+                        }
+                    }
+                },
+                _ => {}
             }
         }
 
-        if tmp
+        for i in 0..problem.1.len()
         {
-            rec.solutions.remove(i);
-            break;
+            ret += (*pattern_2_num_m.get_by_left(&problem.1[i]).unwrap() as u64) * 10_u64.pow(3 - i as u32);
         }
     }
 
     ret
+
 }
 
 fn main()
 {
-    // let filename = "src/bin/day08/input.txt";
-    let filename = "src/bin/day08/example_input.txt";
-
     let start = Instant::now();
+
+    let filename = "src/bin/day08/input.txt";
+    // let filename = "src/bin/day08/example_input.txt";
 
     let mut problems = parse_input(filename);
     println!("part 1 {}", count_unique(&problems));
 
-    for problem in &mut problems
-    {
-        solve_problem(problem);
-    }
+
+    let sum = solve_problems_2(&problems);
+    println!("part 2 {}", sum);
+
 
     let end = Instant::now();
     println!("Took: {:?}", end.duration_since(start));
