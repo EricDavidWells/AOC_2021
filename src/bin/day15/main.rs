cd7de1cc1374203beb4decfbc2e72f4a79fda2f7
eric <ewells@ualberta.ca>
2022-01-09 23:44:01 -0800

    day15 pt2

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -7,8 +7,9 @@ use regex::Regex;
 use petgraph::{Graph, Undirected};
 use petgraph::prelude::*;
 use petgraph::algo::{astar, dijkstra};
+use ndarray::{Array2, Zip, Axis};
 
-fn parse_input(filename: &str) -> (GraphMap<(usize, usize), u64, Directed>, Vec<(usize, usize)>)
+fn parse_input(filename: &str) -> Array2<usize>
 {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
@@ -19,29 +20,90 @@ fn parse_input(filename: &str) -> (GraphMap<(usize, usize), u64, Directed>, Vec<
     let len = mats.count();
 
 
-    let mut graph : GraphMap<(usize, usize), u64, Directed> = GraphMap::new();
+    // let mut graph : GraphMap<(usize, usize), u64, Directed> = GraphMap::new();
+    // let mut nodes: Vec<(usize, usize)> = Vec::new();
 
-    let mut nodes: Vec<(usize, usize)> = Vec::new();
+    let mut weights: Array2<usize> = Array2::from_elem((len, len), 0);
 
     for (i, mat) in re.find_iter(contents.as_str()).enumerate()
     {
         for (j, val) in mat.as_str().bytes().enumerate()
         {
+            weights[[i, j]] = val as usize - 48;
+        }
+    }
+
+    weights
+}
+
+fn make_graph_from_weights(weights: &Array2<usize>) -> (GraphMap<(usize, usize), u64, Directed>, Vec<(usize, usize)>)
+{
+    let mut graph : GraphMap<(usize, usize), u64, Directed> = GraphMap::new();
+    let mut nodes: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..weights.nrows()
+    {
+        for j in 0..weights.ncols()
+        {
             nodes.push(graph.add_node((i, j)));
             add_edges_to_fully_connected_graph_with_this_is_getting_to_be_a_long_name_not_sure_what_to_do_about_it_no_diagonals_though(
-                &mut graph, (i, j), val as u64 - 48, len
+                &mut graph, (i, j), weights[[i, j]] as u64, weights.nrows()
             );
         }
     }
+
     (graph, nodes)
 }
 
-fn expand_graph(graph: &mut GraphMap<(usize, usize), u64, Directed>, nodes: &mut Vec<(usize, usize)>, n: usize)
+// this is pretty trash and confusing but it works
+fn expand_weights(weights: &mut Array2<usize>, n: usize) -> Array2<usize>
 {
+
+    let mut ret: Array2<usize> = weights.clone();
+
+    let mut row_start_vals: Array2<usize> = weights.clone();
+
     for i in 0..n
     {
+        let mut next_block_vals = row_start_vals.clone();
+        let mut row_agregator = next_block_vals.clone();
 
+        for j in 1..n
+        {
+            Zip::from(&mut next_block_vals)
+                .for_each(|x| {
+                    *x = ((*x + 1) - 1)%9 + 1;
+                    if *x > 9
+                    {
+                        *x = 1;
+                    }
+                });
+
+            row_agregator.append(Axis(1), next_block_vals.view()).unwrap();
+
+        }
+
+        // increment new weights
+        Zip::from(&mut row_start_vals)
+            .for_each(|x| {
+                *x = ((*x + 1) - 1)%9 + 1;
+                if *x > 9
+                {
+                    *x = 1;
+                }
+            });
+
+        if i == 0
+        {
+            ret = row_agregator.clone();
+        }
+        else
+        {
+            ret.append(Axis(0), row_agregator.view()).unwrap();
+        }
     }
+
+    ret
 }
 
 fn add_edges_to_fully_connected_graph_with_this_is_getting_to_be_a_long_name_not_sure_what_to_do_about_it_no_diagonals_though
@@ -73,13 +135,17 @@ fn main()
     let filename = "src/bin/day15/input.txt";
     // let filename = "src/bin/day15/example_input.txt";
 
-    let (mut graph, mut nodes) = parse_input(filename);
+    let mut weights = parse_input(filename);
+    let (mut graph, mut nodes) = make_graph_from_weights(&weights);
+
     let path = astar(&graph, *nodes.first().unwrap(), |finish| finish == *nodes.last().unwrap(), |e| *e.weight(), |_| 0);
     println!("Cost was: {}", path.unwrap().0);
 
-    expand_graph(&mut graph, &mut nodes, 5);
-    let path = astar(&graph, *nodes.first().unwrap(), |finish| finish == *nodes.last().unwrap(), |e| *e.weight(), |_| 0);
-    println!("Cost was: {}", path.unwrap().0);
+    let new_weights = expand_weights(&mut weights, 5);
+    let (mut graph2, mut nodes2) = make_graph_from_weights(&new_weights);
+
+    let path2 = astar(&graph2, *nodes2.first().unwrap(), |finish| finish == *nodes2.last().unwrap(), |e| *e.weight(), |_| 0);
+    println!("Cost was: {}", path2.unwrap().0);
 
     let end = Instant::now();
     println!("Took: {:?}", end.duration_since(start));
